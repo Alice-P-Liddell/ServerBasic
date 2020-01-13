72f79200c1347850d9074d9e6cc029d8c25d2902
Daehan Lee <alice.p.liddell0v0@gmail.com>
2020-01-13 14:12:26 +0900

    final
    종강시점 저장

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var mongodb = require('mongodb');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -22,27 +24,110 @@ router.post('/signup', function(req, res, next) {
   var validate = userValidation(username, password);
   if (validate == false) {
     res.json({message:'400 Bad Request'});
+    return;
   }
 
   var usersCollection = db.collection('users');
 
-  usersCollection.count({'username':username}, function(err, result) {
+  usersCollection.count({username:username}, function(err, result) {
     if (err) throw(err);
 
     if (result > 0) {
       res.json({message: '400 Bad Request'});
       return;
     } else {
-      usersCollection.insertOne({'username': username, 
-      'password': password, 'name': name}, function(err, result) {
-        if (err) throw(err);
-        if (result.ops.length > 0)
-          res.json(result.ops[0]);
-        else
-          res.json({message:'503 Server Error'});
+      crypto.randomBytes(64, function(err, buf) {
+        const saltStr = buf.toString('base64');
+        crypto.pbkdf2(password, saltStr, 102391, 64, 'sha512', function(err, key) {
+          const cryptoPassword = key.toString('base64');
+    
+          usersCollection.insertOne({username: username, password: cryptoPassword, 
+            name: name, salt: saltStr, score: 0}, function(err, result) {
+            if (err) throw(err);
+            if (result.ops.length > 0) {
+              res.json({message: '200 ok'});
+            } else {
+              res.json({message:'503 Server Error'});
+            }
+          });
+        });  
       });
     }
   });
+});
+
+/* 로그아웃 */
+router.get('/logout', function(req, res, next) {
+  req.session.destroy(function(err) {
+    res.clearCookie('connect.sid');
+    res.json({message:'200 Ok'});
+  });
+});
+
+/* 로그인 */
+router.post('/signin', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  var db = req.app.get('database');
+
+  if (db == undefined) {
+    res.json({message:'503 Server Error'});
+    return;
+  }
+
+  var usersCollection = db.collection('users');
+
+  usersCollection.findOne({username: username}, function(err, result) {
+    if (err) throw(err);
+
+    if (result) {
+      var saltStr = result.salt;
+      crypto.pbkdf2(password, saltStr, 102391, 64, 'sha512', function(err, key) {
+        var cryptoPassword = key.toString('base64');
+
+        usersCollection.findOne({username: username, password: cryptoPassword}, 
+          function(err, result) {
+            if (err) throw(err);
+            if (result) {
+              req.session.user_id = result._id.toString();
+              res.json({message:'200 Ok'});
+            } else {
+              res.json({message:'204 No Content'});
+            }
+          });
+      });
+    } else {
+      res.json({message:'204 No Content'});
+    }
+  });
+});
+
+// 점수 추가
+router.post('/addscore', function(req, res, next) {
+  var score = req.body.score;
+
+  var db = req.app.get('database');
+  if (db == undefined) {
+    res.json({message: '503 Server Error'});
+    return;
+  }
+
+  var userId = req.session.user_id;
+
+  if (userId) {
+    var usersCollection = db.collection('users');
+    usersCollection.update({_id:mongodb.ObjectID(userId)}, {$inc:{score:score}}, function(err, result) {
+      if (err) throw(err);
+      if (result) {
+        res.json({message:'200 Ok'});
+      } else {
+        res.json({message:'204 No Content'});
+      }
+    });
+  } else {
+    res.json({message:'401 Unauthorized'})
+  }
 });
 
 var userValidation = function(username, password) {
@@ -57,10 +142,5 @@ var userValidation = function(username, password) {
   }
   return true;
 }
-
-/* 로그인 */
-router.post('/signin', function(req, res, next) {
-
-});
 
 module.exports = router;
